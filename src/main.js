5d69e06195a66a76fc7aba51e70b95b3fa65f7d4
TsotneBasili <137988770+TsotneBasili@users.noreply.github.com>
2024-11-12 18:25:06 +0400

    Update main.js
    semi coloms

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-num1=4
-num2=5
-num3=0
+num1=4;
+num2=5;
+num3=0;
 console.log(num1+num2);
 console.log(num1*num2*num3);
-my_name="Tsotne"
+my_name="Tsotne";
 console.log("hello " + my_name);
 
 console.log("good " + "morning");
