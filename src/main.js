0b4c16d60b1630b228b84b6131d047944a8b835c
TsotneBasili <137988770+TsotneBasili@users.noreply.github.com>
2024-11-12 18:21:56 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-// Write your code here
+num1=4
+num2=5
+num3=0
+console.log(num1+num2);
+console.log(num1*num2*num3);
+my_name="Tsotne"
+console.log("hello " + my_name);
+
+console.log("good " + "morning");
